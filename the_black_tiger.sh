4adf76462ad02cb916c4e8433a717a5637e08ef1
VirusZzHkP <hrisikeshpal2001@gmail.com>
2024-05-06 11:57:48 +0530

    fixed menu

--- the_black_tiger.sh
+++ the_black_tiger.sh
@@ -7,16 +7,6 @@ echo "                                [0] ==>Install and Configure Requirements<
 echo "                                [1] Information about a Website/Company         🔍"
 echo "                                [2] Information of a Person                     🔍"
 echo "                                [3] Information about a Nickname/Alias/Nickname 🔍"
-<<<<<<< HEAD
-echo "                                [4] Information from a Social Network          🔍"
-echo "                                [5] Email Information                🔍"
-echo "                                [6] Telephone Number Information   🔍"
-echo "                                [7] Public IP Information          🔍"
-echo "                                [8] YouTube Video/Channel Information  🔍"
-echo "                                [9] Image Information            🔍"
-echo "                                [10] Record Search            🔍"
-echo "                                [99] -------------> Exit"" <--------------- 🔍"
-=======
 echo "                                [4] Information from a Social Network           🔍"
 echo "                                [5] Email Information                           🔍"
 echo "                                [6] Telephone Number Information                🔍"
@@ -25,7 +15,6 @@ echo "                                [8] YouTube Video/Channel Information     
 echo "                                [9] Image Information                           🔍"
 echo "                                [10] Record Search                              🔍"
 echo "                                [99] ----------------> Exit"" <------------------ "
->>>>>>> a65ec0982235d4bd77a8fb00e139405a06eed26b
 echo
 read -p "[*] Choose an option: " opc1
 	case $opc1 in
@@ -49,16 +38,9 @@ read -p "[*] Choose an option: " opc1
 				;;
 			9 )	bash requirements/9.sh
 				;;
-<<<<<<< HEAD
-			10 )	bash requirements/10.sh
-				;;
-			99 ) echo "Thanks for using 'the black tiger', made with love VirusZzWarning."
-				exit
-=======
 			10 ) bash requirements/10.sh
 				;;
-			99 )	exit
->>>>>>> a65ec0982235d4bd77a8fb00e139405a06eed26b
+			99 ) echo "Thanks for using The-Black-tiger!! M@d3 With Love - VirusZzWarning"	exit
 				;;
 			* )	echo
 				echo "$RRPLY It is not a valid option"
